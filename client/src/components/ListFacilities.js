f1cbec62959915cc62527a76cc0c30b76737e2e9
Amy Yang <hpfanamy@gmail.com>
2018-04-22 13:05:46 -0700

    list facilities

---
+++ client/src/components/ListFacilities.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+class ListFacilites extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      facilities: []
+    }
+  }
+
+  componentWillMount(){
+    this.getFacilities();
+  }
+
+  getFacilities(){
+    axios.get('/api/facilities')
+      .then(res => {
+        this.setState({facilities: res.data}, () =>{
+          // console.log(this.state)
+        })
+      })
+      .catch(err => console.log(err));
+  } 
+
+  render() {
+    const facilityItems = this.state.facilities;
+    return (
+      <div>
+        <h1>Facilities</h1>
+        {facilityItems.map((facility, i) => {
+          return (
+            <li key={i}>
+              <Link to={`/issues/facility-issues/${facility.name}`} >
+                {facility.name}
+              </Link>
+              
+            </li>
+          )
+        })}
+      </div>
+    );
+  }
+}
+
+export default Issues;
