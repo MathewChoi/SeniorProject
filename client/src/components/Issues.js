01f03e69ddf7c55bbbb2799b8ac8e736e6e5a2a1
Amy Yang <hpfanamy@gmail.com>
2018-05-02 12:37:26 -0700

    ui changes for issues

--- client/src/components/Issues.js
+++ client/src/components/Issues.js
@@ -27,33 +27,73 @@ class Issues extends Component {
       .catch(err => console.log(err));
   } 
 
+  onDelete(id){
+    if (window.confirm("Are you sure?")) {
+      axios.delete('/api/issues/'+id)
+      .then((result) => {
+        window.location.reload();
+      });
+    } 
+  }
+
   render() {
     const issueItems = this.state.issues;
     return (
-      <div>
-        <h1>Issues</h1>
-        {issueItems.map((issue, i) => {
-          var createdAt = moment(issue.createdAt).format('MM-DD-YYYY h:mm:ss a');
-          
-          return (
-            <div className="dark" key={i}>
-              <Link to={`/issues/${issue._id}`} className="title">
-                {issue.name}
-              </Link>
-              <p className="spacing" >Description: {issue.description}</p>
-              <div className="location">
-                <p className="spacing">Building: {issue.building}</p>
-                <p className="spacing">Floor: {issue.floor}</p>
-                <p className="spacing">Room: {issue.room}</p>
-              </div>
-              <p className="spacing">Category: {issue.category}</p>
-              <p className="spacing">Status: {issue.status}</p>
-              <p className="spacing">Created At: {createdAt}</p>
-        
+      <div className="card">
+        <div className="card-header card-header-primary">
+            <h4 className="card-title ">View all issues</h4>
+        </div>
+        <div className="card-body">
+            <div className="table-responsive">
+                <table className="table">
+                    <thead className=" text-primary">
+                        <th>
+                         Issue Name   
+                        </th>
+                        <th>
+                            Description
+                        </th>
+                        <th>
+                            Building
+                        </th>
+                        <th>
+                            Floor
+                        </th>
+                        <th>
+                          Room
+                        </th>
+                        <th>
+                            Category
+                        </th>
+                        <th>
+                          Date Created
+                        </th>
+                    </thead>
+                    <tbody>
+                    {issueItems.map((issue, i) => {
+                      var createdAt = moment(issue.createdAt).format('MM-DD-YYYY h:mm:ss a');
+                return (
+                  <tr key={i}>
+                    <td>{issue.name}</td>
+                    <td>{issue.description}</td>
+                    <td>{issue.building}</td>
+                    <td>{issue.floor}</td>
+                    <td>{issue.room}</td>
+                    <td>{issue.category}</td>
+                    <td>{createdAt}</td>
+                    <td>
+                      <Link to={`/issues/${issue._id}`}>View</Link> | <Link to={`/issues/update/${issue._id}`}>Edit</Link> | <button onClick={this.onDelete.bind(this, issue._id)} className="btn btn-outline-danger">Delete</button>
+                    </td>
+                  </tr>
+                )
+              })}
+                        
+                    </tbody>
+                </table>
             </div>
-          )
-        })}
-      </div>
+        </div>
+    </div>
+
     );
   }
 }
--- client/src/components/Issues/FacilityIssues.js
+++ client/src/components/Issues/FacilityIssues.js
@@ -41,15 +41,23 @@ class FacilityIssues extends Component {
       })
       .catch(err => console.log(err));
   }
+  onDelete(id){
+    if (window.confirm("Are you sure?")) {
+      axios.delete('/api/issues/'+id)
+      .then((result) => {
+        window.location.reload();
+      });
+    } 
+  }
   
   render() {
     const issueItems = this.state.issues;
     const url= window.location.pathname.split('/');
     return (
-      <div>
-        <h1>{url[3]}</h1>
-
-        <select className="form-control" value={this.state.query} onChange={this.onChange}>
+      <div className="card">
+        <div className="card-header card-header-primary">
+            <h4 className="card-title ">{url[3]}</h4>
+            <select className="form-control" value={this.state.query} onChange={this.onChange}>
               <option value='?sort=createdAt&order=-1'>Sort by recently reported</option>
               <option value='?sort=createdAt&order=1'>Sort by earliest reported</option>
               <option value='?sort=name&order=1'>Sort by issues in alphabetical order (A-Z)</option>
@@ -57,32 +65,58 @@ class FacilityIssues extends Component {
               <option value='?sort=floor&order=1'>Sort by floor (LOW to HIGH)</option>
               <option value='?sort=floor&order=-1'>Sort by floor(HIGH to LOW)</option>
             </select>
-
-
-
-
-        {issueItems.map((issue, i) => {
-          var createdAt = moment(issue.createdAt).format('MM-DD-YYYY h:mm:ss a');
-          
-          return (
-            <div className="dark" key={i}>
-              <Link to={`/issues/${issue._id}`} className="title">
-                {issue.name}
-              </Link>
-              <p className="spacing" >Description: {issue.description}</p>
-              <div className="location">
-                <p className="spacing">Building: {issue.building}</p>
-                <p className="spacing">Floor: {issue.floor}</p>
-                <p className="spacing">Room: {issue.room}</p>
-              </div>
-              <p className="spacing">Category: {issue.category}</p>
-              <p className="spacing">Status: {issue.status}</p>
-              <p className="spacing">Created At: {createdAt}</p>
-        
+        </div>
+        <div className="card-body">
+            <div className="table-responsive">
+                <table className="table">
+                    <thead className=" text-primary">
+                        <th>
+                         Issue Name   
+                        </th>
+                        <th>
+                            Description
+                        </th>
+                        <th>
+                            Building
+                        </th>
+                        <th>
+                            Floor
+                        </th>
+                        <th>
+                          Room
+                        </th>
+                        <th>
+                            Category
+                        </th>
+                        <th>
+                          Date Created
+                        </th>
+                    </thead>
+                    <tbody>
+                    {issueItems.map((issue, i) => {
+                      var createdAt = moment(issue.createdAt).format('MM-DD-YYYY h:mm:ss a');
+                return (
+                  <tr key={i}>
+                    <td>{issue.name}</td>
+                    <td>{issue.description}</td>
+                    <td>{issue.building}</td>
+                    <td>{issue.floor}</td>
+                    <td>{issue.room}</td>
+                    <td>{issue.category}</td>
+                    <td>{createdAt}</td>
+                    <td>
+                      <Link to={`/issues/${issue._id}`}>View</Link> | <Link to={`/issues/update/${issue._id}`}>Edit</Link> | <button onClick={this.onDelete.bind(this, issue._id)} className="btn btn-outline-danger">Delete</button>
+                    </td>
+                  </tr>
+                )
+              })}
+                        
+                    </tbody>
+                </table>
             </div>
-          )
-        })}
-      </div>
+        </div>
+    </div>
+
     );
   }
 }
